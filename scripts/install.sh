cb6bb741204853f896c0fb91d511ef3acaf31fc4
verafhal <verafhal@gmail.com>
2025-05-28 00:46:50 +0300

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+
+# Fix ownership and permissions
+sudo chown -R ubuntu:ubuntu /home/ubuntu/myapp
+sudo chmod -R u+rwX /home/ubuntu/myapp
+
+# Move into app directory
 cd /home/ubuntu/myapp
+
+# Install dependencies
 npm install
+
