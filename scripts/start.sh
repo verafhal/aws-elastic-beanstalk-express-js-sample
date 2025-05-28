2875076082885ba68f1aedfb0a8ec924230ac4af
verafhal <verafhal@gmail.com>
2025-05-28 10:02:46 +0300

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 cd /home/ubuntu/myapp
+
+# Stop and delete existing PM2 app (if running)
+pm2 delete myapp || true
+
+# Start app with PM2
 pm2 start app.js --name "myapp"
+
+# Save PM2 process list (for resurrecting on reboot)
+pm2 save
